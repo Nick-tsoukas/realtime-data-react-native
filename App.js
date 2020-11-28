67edc0a30f298e10307a74192b9dcf6f68b1feff
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-11-28 13:57:21 -0600

    Adding state and text input ... refactored using hooks

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import React, { Component, useEffect, useState, setState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 import io from 'socket.io-client';
 
-export default class App extends Component {
-  componentDidMount(){
-    const socket = io("http://973759e5e99b.ngrok.io");
+const App = () => {
+  const [value, onChangeText] = useState('');
+
+  const log = async () => {
+    await console.log(value);
+    onChangeText('')
   }
+
   
-  render(){
+    useEffect(() => {
+      const socket = io("http://6cc239e13b44.ngrok.io");
+      console.log('this is the use effect hook')
+    },[]);
+
     return(
       <View style={styles.screen}>
-        <Text>Hello This is the home screen</Text>
+        <TextInput
+          style={{ height: 40, width:100, borderColor: 'black', marginBottom:20, borderWidth: 1 }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+        <Button
+        style={{width: '100%'}}
+        title="Click Me"
+        onPress={log}
+        />
       </View>
     )
-  }
-};
+  };
 
 const styles = StyleSheet.create({
  screen : {
@@ -23,4 +40,6 @@ const styles = StyleSheet.create({
    alignItems: 'center'
  }
 });
+
+export default App
 
